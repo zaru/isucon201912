02accea38ccb10e2bb70266d414dc3e6a7c2a7a2
zaru <zarutofu@gmail.com>
2019-12-23 23:33:25 +0900

    puma

--- Gemfile
+++ Gemfile
@@ -6,5 +6,5 @@ gem 'mysql2-cs-bind'
 gem 'erubis'
 gem 'rack-cache'
 gem 'redis'
-gem "unicorn"
+gem 'puma'
 gem 'activesupport'
--- puma.rb
+++ puma.rb
@@ -4,8 +4,8 @@ bind "unix:///tmp/unicorn.sock"
 pidfile "#{root}/unicorn.pid"
 state_path "/tmp/state"
 rackup "#{root}/config.ru"
-threads 4, 8
+threads 4, 16
 activate_control_app
 daemonize true
-workers 3
+workers 2
 preload_app!
